ca7155f52b4937b48777ed949375a4298a47b1da
seanmars <mars0222@gmail.com>
2022-10-04 07:51:18 +0800

    First commit

---
+++ TimelapseApp/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var targetUrl = "";
+
+try
+{
+    var httpClient = new HttpClient();
+    var uri = new Uri(targetUrl);
+    var stream = await httpClient.GetStreamAsync(uri);
+
+    var timeOffset = new DateTimeOffset(DateTime.Now, TimeSpan.FromHours(8));
+    var fileName = $"./{timeOffset:yyyyMMddHHmm}.jpeg";
+    Console.WriteLine(fileName);
+    await using var fs = new FileStream(fileName, FileMode.CreateNew);
+    await stream.CopyToAsync(fs);
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.ToString());
+}
